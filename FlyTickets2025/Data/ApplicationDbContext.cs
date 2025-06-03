789a0f4c789fb728fc45a882a09220eb848eef1f
specbit <nmiguelgomes77@gmail.com>
2025-06-03 19:18:55 +0100

    Add project files.

---
+++ FlyTickets2025/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyTickets2025.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
